83b5743e3e96b0eedbf385d414f78b2fdbaa22ec
jkuusist <jkuusist@student.hive.fi>
2020-01-29 12:35:11 +0200

    implemented basic skeleton of the function

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,35 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 16:21:54 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/01/28 16:37:40 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/01/29 12:34:43 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
+void	handle_specifier(char *str)
+{
+
+}
+
 int		printf(const char *format, ...)
 {
 	va_list ap;
-	int		num_args;
 
-	//get num_args here
-
-	va_start(ap, num_args);
-
+	va_start(ap, format);
+	formatcpy = format;
+	while (*format != '\0')
+	{
+		while (*format != '%')
+		{
+			ft_putchar(*format);
+			format++;
+		}
+		format++;
+		handle_specifier(*format);
+	}
+	
+	va_end(arg);
 	//These functions return the number of characters printed (not including the trailing `\0')
 	//These functions return a negative value if an error occurs.
 }
