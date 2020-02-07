53fe9d14dc7b4a2ffdbe82b298d6d83f63766d12
jkuusist <jkuusist@student.hive.fi>
2020-02-07 12:51:35 +0200

    added handling for strings in basic cases

--- srcs/handle_specifier.c
+++ srcs/handle_specifier.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 13:23:11 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/02/07 12:32:45 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/02/07 12:48:45 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	handle_specifier(t_struct *new)
 {
 	if ((new->format)[new->index] == 'c' || (new->format)[new->index] == 'C')
 	   ft_putchar(va_arg(new->ap, char));
+	if ((new->format)[new->index] == 's')
+		ft_putstr(va_arg(new->ap, char*));
 	if ((new->format)[new->index] == 'd' || (new->format)[new->index] == 'D')
 		ft_putnbr(va_arg(new->ap, int));
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,8 +6,9 @@ int main()
 {
 	char ch1 = 'M';
 	int in1 = -0;
+	char *str = ";rmgs;gks   twjj;snfggsn 			#$%%@WLSRFSL";
 	
-	int ret_val = ft_printf("ch1 is: %c\nin1 is: %d\n", ch1, in1);
+	int ret_val = ft_printf("ch1 is: %c\nin1 is: %d\nstr is: %s\n", ch1, in1, str);
 	
 	//printf("\nret_val is %d\n", ret_val);
 
