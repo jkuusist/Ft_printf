cff6c8a9cfdebdcc9a69a24db087804685eb5b0e
jkuusist <jkuusist@student.hive.fi>
2020-02-14 13:32:43 +0200

    printing of doubles now seems to work same as standard printf

--- srcs/handle_specifier.c
+++ srcs/handle_specifier.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 13:23:11 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/02/13 11:38:50 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/02/14 13:18:45 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 #include "../includes/ft_printf.h"
 #include "../Libft/libft.h"
 
+#include <stdio.h>
+
 void	handle_specifier(t_struct *new)
 {
+	char temp[20];
+
 	if ((new->format)[new->index] == 'c' || (new->format)[new->index] == 'C')
 	   ft_putchar(va_arg(new->ap, char));
 	if ((new->format)[new->index] == 's')
@@ -23,7 +27,11 @@ void	handle_specifier(t_struct *new)
 	if ((new->format)[new->index] == 'd' || (new->format)[new->index] == 'D')
 		ft_putnbr(va_arg(new->ap, int));
 	if ((new->format)[new->index] == 'f' || (new->format)[new->index] == 'F')
-		print_double(va_arg(new->ap, double), 6);
+	{
+		//printf("%f\n", va_arg(new->ap, double));
+		double_to_str(va_arg(new->ap, double), temp, 6);
+		ft_putstr(temp);
+	}
 		//6 is the default precision of the real printf
 		//TBA: handling for different precisions
 }
--- srcs/print_double.c
+++ srcs/print_double.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 11:20:21 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/02/14 12:00:46 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/02/14 13:31:13 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 #include <stdio.h>
 
-int		int_to_str(int num, char *str, int precision)
+int		int_to_str(int num, char *str, int precision, int is_negative)
 {
 	int i;
 
+	
 	i = 0;
 	while (num)
 	{
@@ -28,6 +29,11 @@ int		int_to_str(int num, char *str, int precision)
 	while (i < precision)
 	{
 		str[i] = '0';
+		i++;
+	}
+	if (is_negative)
+	{
+		str[i] = '-';
 		i++;
 	}
 	ft_strrev(str, i);
@@ -43,16 +49,15 @@ void	double_to_str(double dub, char *result, int precision)
 
 	int_part = (int)dub;
 	frac_part = dub - (double)int_part;
-	
 	if (dub < 0)
 	{
-		*result = '-';
-		result++;
+		//result[0] = '-';
 		int_part *= -1;
 		frac_part *= -1;
+		i = int_to_str(int_part, result, 0, 1);
 	}
 	if (dub > 1.0)
-		i = int_to_str(int_part, result, 0);
+		i = int_to_str(int_part, result, 0, 0);
 	if ((dub < 1.0) && (dub > 0.0))
 	{
 		result[0] = '0';
@@ -64,23 +69,8 @@ void	double_to_str(double dub, char *result, int precision)
 		frac_part = frac_part * ft_pow(10, precision);
 		result = result + i + 1;
 		if ((int)(frac_part + 0.5))
-			int_to_str((int)(frac_part + 0.5), result, precision);
+			int_to_str((int)(frac_part + 0.5), result, precision, 0);
 		else
-			int_to_str((int)frac_part, result, precision);
+			int_to_str((int)frac_part, result, precision, 0);
 	}
-}
-
-int main()
-{
-	char string[20];
-	double doo = 1.61803398;
-//	double doo = 233.107;
-	//double doo = 0.847238428;
-	printf("printf: %lf\n", doo);
-
-	double_to_str(doo, string, 6);
-
-	printf("%s\n", string);
-
-	return (0);
 }
