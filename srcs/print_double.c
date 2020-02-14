0e8339f77004063f031b075c7ea8d90f08730a1c
jkuusist <jkuusist@student.hive.fi>
2020-02-14 12:02:23 +0200

    added proper rounding, and things work properly when the integer part is 0

--- srcs/print_double.c
+++ srcs/print_double.c
@@ -6,15 +6,13 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 11:20:21 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/02/14 11:29:37 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/02/14 12:00:46 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Libft/libft.h"
 
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int		int_to_str(int num, char *str, int precision)
 {
@@ -53,47 +51,36 @@ void	double_to_str(double dub, char *result, int precision)
 		int_part *= -1;
 		frac_part *= -1;
 	}
-
-	i = int_to_str(int_part, result, 0);
-
-	//printf("i is now %d\n", i);
-	//printf("frac_part is now %lf\n", frac_part);
-	//ft_putnbr(int_part);
-
+	if (dub > 1.0)
+		i = int_to_str(int_part, result, 0);
+	if ((dub < 1.0) && (dub > 0.0))
+	{
+		result[0] = '0';
+		i = 1;
+	}
 	if (precision != 0)
 	{
-		//ft_putchar('.');
-
 		result[i] = '.';
 		frac_part = frac_part * ft_pow(10, precision);
-
-		//printf("result is: %s\nresult + i is: %s\n", result, result + i);
-
 		result = result + i + 1;
-		int_to_str((int)frac_part, result, precision);
-
-		printf("frac_part is now %lf\n", frac_part);
-
-		//ft_putnbr((int)frac_part);
+		if ((int)(frac_part + 0.5))
+			int_to_str((int)(frac_part + 0.5), result, precision);
+		else
+			int_to_str((int)frac_part, result, precision);
 	}
-
-
 }
 
 int main()
 {
-	//double doo = -1.61803398;
-	//char *s = (char*)malloc(sizeof(char) * 20);
 	char string[20];
-	double doo = -233.107;
-
-	//printf("printf: %lf\n", doo);
+	double doo = 1.61803398;
+//	double doo = 233.107;
+	//double doo = 0.847238428;
+	printf("printf: %lf\n", doo);
 
 	double_to_str(doo, string, 6);
 
 	printf("%s\n", string);
-
-	//free(s);
 
 	return (0);
 }
