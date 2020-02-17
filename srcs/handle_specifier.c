ceda6e8e1fbd10e11b40d894513c9745954e9639
jkuusist <jkuusist@student.hive.fi>
2020-02-17 14:24:48 +0200

    added handling for the oOxX specifiers

--- srcs/handle_specifier.c
+++ srcs/handle_specifier.c
@@ -6,19 +6,19 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 13:23:11 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/02/14 13:56:28 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/02/17 14:12:40 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stdlib.h>
 #include "../includes/ft_printf.h"
 #include "../Libft/libft.h"
-
-#include <stdio.h>
 
 void	handle_specifier(t_struct *new)
 {
 	char temp[20];
+	char *s;
 
 	if ((new->format)[new->index] == 'c' || (new->format)[new->index] == 'C')
 	   ft_putchar(va_arg(new->ap, char));
@@ -28,10 +28,30 @@ void	handle_specifier(t_struct *new)
 		ft_putnbr(va_arg(new->ap, int));
 	if ((new->format)[new->index] == 'f' || (new->format)[new->index] == 'F')
 	{
-		//printf("%f\n", va_arg(new->ap, double));
 		double_to_str(va_arg(new->ap, double), temp, 6);
 		ft_putstr(temp);
 	}
+	if ((new->format)[new->index] == 'o' || (new->format)[new->index] == 'O')
+	{
+		s = ft_itoa_base(va_arg(new->ap, int), 8);
+		ft_putstr(s);
+		free(s);
+	}
+	if ((new->format)[new->index] == 'x')
+	{
+		s = ft_strlower(ft_itoa_base(va_arg(new->ap, int), 16));
+		ft_putstr(s);
+		free(s);
+	}
+	if ((new->format)[new->index] == 'X')
+	{
+		s = ft_itoa_base(va_arg(new->ap, int), 16);
+		ft_putstr(s);
+		free(s);
+	}
+
+
+		
 		//6 is the default precision of the real printf
 		//TBA: handling for different precisions
 }
