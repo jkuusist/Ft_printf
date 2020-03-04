57e342a8e2f502b380d72493c39fd1c55313e919
jkuusist <jkuusist@student.hive.fi>
2020-03-04 15:08:58 +0200

    changed ft_pow to be non-recursive and return a long insteaad of an int

--- Libft/ft_pow.c
+++ Libft/ft_pow.c
@@ -6,14 +6,21 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 16:03:36 by jkuusist          #+#    #+#             */
-/*   Updated: 2020/02/10 16:16:23 by jkuusist         ###   ########.fr       */
+/*   Updated: 2020/03/04 15:08:15 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_pow(int base, unsigned int exp)
+long		ft_pow(int base, unsigned int exp)
 {
-	if (exp != 0)
-		return (base * ft_pow(base, (exp - 1)));
-	else
-		return (1);
+	unsigned int	i;
+	long			res;
+
+	i = 0;
+	res = 1;
+	while (i < exp)
+	{
+		res *= base;
+		i++;
+	}
+	return (res);
 }
