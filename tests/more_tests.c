e6f1163dbc1ac0dc9c6788cd5eeff9fb9b6fb02d
jkuusist <jkuusist@student.hive.fi>
2020-06-17 13:46:09 +0200

    started adding tests from 42filechecker for what will hopefully be the final push

--- tests/more_tests.c
+++ tests/more_tests.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-
+/*
 	int r1 = printf("%d\n", -4242);
 	int r2 = ft_printf("%d\n", -4242);
 	printf("real is %d\nmine is %d\n", r1, r2);
@@ -60,6 +60,76 @@ int main()
 	int r20 = ft_printf("%0+5d\n", -42);
 	printf("real is %d\nmine is %d\n", r19, r20);
 	printf("\n");
+*/
+	
+	int r21 = printf("|%-5o|\n", 2500);
+	int r22 = ft_printf("|%-5o|\n", 2500);	
+	printf("real is %d. mine is %d\n\n", r21, r22);
+
+	int r23 = printf("|%-#6o|\n", 2500);
+	int r24 = ft_printf("|%-#6o|\n", 2500);
+	printf("real is %d. mine is %d\n\n", r23, r24);
+
+	int r25 = printf("|%-05o|\n", 2500);
+	int r26 = ft_printf("|%-05o|\n", 2500);
+	printf("real is %d. mine is %d\n\n", r25, r26);
+
+	int r27 = printf("% d\n", -42);
+	int r28 = ft_printf("% d\n", -42);
+	printf("real is %d. mine is %d\n\n", r27, r28);
+	
+	int r29 = printf("% +d\n", -42);
+	int r30 = ft_printf("% +d\n", -42);
+	printf("real is %d. mine is %d\n\n", r29, r30);
+
+	int r31 = printf("%+ d\n", -42);
+	int r32 = ft_printf("%+ d\n", -42);
+	printf("real is %d. mine is %d\n\n", r31, r32);
+
+	int r33 = printf("%  +d\n", -42);
+	int r34 = ft_printf("%  +d\n", -42);
+	printf("real is %d. mine is %d\n\n", r33, r34);
+
+	int r35 = printf("%+  d\n", -42);
+	int r36 = ft_printf("%+  d\n", -42);
+	printf("real is %d. mine is %d\n\n", r35, r36);
+
+	int r37 = printf("% ++d\n", -42);
+	int r38 = ft_printf("% ++d\n", -42);
+	printf("real is %d. mine is %d\n\n", r37, r38);
+
+	int r39 = printf("%++ d\n", -42);
+	int r40 = ft_printf("%++ d\n", -42);
+	printf("real is %d. mine is %d\n\n", r39, r40);
+
+	int r41 = printf("%5d\n", -42);
+	int r42 = ft_printf("%5d\n", -42);
+	printf("real is %d. mine is %d\n\n", r41, r42);
+	
+	int r43 = printf("%hhd\n", 128);
+	int r44 = ft_printf("%hhd\n", 128);
+	printf("real is %d. mine is %d\n\n", r43, r44);
+
+	int r45 = printf("%hhd", -129);
+	int r46 = ft_printf("%hhd", -129);
+	printf("real is %d. mine is %d\n\n", r45, r46);
+
+	int r47 = printf("%lld", -9223372036854775808);
+	int r48 = ft_printf("%lld", -9223372036854775808);
+	printf("real is %d. mine is %d\n\n", r47, r48);
+
+	int r49 = printf("%4.15d", 42);
+	int r50 = ft_printf("%4.15d", 42);
+	printf("real is %d. mine is %d\n\n", r49, r50);
+
+
+
+
+
+
+
+
+
 
 
 	return 0;
